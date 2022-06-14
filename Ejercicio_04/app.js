f6f28a3ee7c14de3188648c804ed3ee0173f31d2
LucioFurnari <luciofurnari99@gmail.com>
2022-06-14 14:11:39 -0300

    Cambio typo

--- Ejercicio_04/app.js
+++ Ejercicio_04/app.js
@@ -2,7 +2,7 @@ const Pizzas = [
     {id: 1, nombre: "Margarita", ingredientes: ["Mozzarella","Salsa","Albahaca","Tomate"],img: "./images/pizza_Margarita.jpg", precio: 500 },
     {id: 2, nombre:"Cuatro quesos", ingredientes: ["Mozzarella","Queso azul","Parmesano","Provolone"],img: "./images/pizza_CuatroQuesos.jpg", precio: 600},
     {id: 3, nombre:"Jamon cocido", ingredientes:["Mozzarella","Salsa","Jamon cocido"],img: "./images/pizza_JamonCocido.jpg", precio: 800},
-    {id: 4, nombre:"Hawaiana", ingredientes:["Mozzarella","Salsa","Jamon","Piña"],img: "./images/pizza_Hawaiana.jpg", precio: 750},
+    {id: 4, nombre:"Hawaiana", ingredientes:["Mozzarella","Salsa","Jamon","Piña"],img: "./images/pizza_hawaiana.jpg", precio: 750},
     {id: 5, nombre:"Champiniones y pepperoni", ingredientes:["Mozzarella","Champiniones","Pepperoni","Tomate"],img: "./images/pizza_PeperoniChampinones.jpg", precio: 650},
     {id: 6, nombre:"Albahaca y cherry", ingredientes:["Mozzarella","Albahaca","Tomate cherry"],img:"./images/pizza_AlbahacaCherry.jpg", precio:550},
 ];
