066616b5f8a16a18cb27f0290b996be29008242e
LucioFurnari <luciofurnari99@gmail.com>
2022-06-14 12:44:14 -0300

    Cambios finales

--- Ejercicio_04/app.js
+++ Ejercicio_04/app.js
@@ -1,10 +1,10 @@
 const Pizzas = [
-    {id: 1, nombre: "Margarita", ingredientes: ["mozzarella","salsa","albahaca","tomate"],img: "./images/pizza_Margarita.jpg", precio: 500 },
-    {id: 2, nombre:"Cuatro quesos", ingredientes: ["mozzarella","queso azul","parmesano","provolone"],img: "./images/pizza_CuatroQuesos.jpg", precio: 600},
-    {id: 3, nombre:"Jamon cocido", ingredientes:["mozzarella","salsa","jamon cocido"],img: "./images/pizza_JamonCocido.jpg", precio: 800},
-    {id: 4, nombre:"Hawaiana", ingredientes:["mozzarella","salsa","jamon","piña"],img: "./images/pizza_Hawaiana.jpg", precio: 750},
-    {id: 5, nombre:"Champiniones y pepperoni", ingredientes:["mozzarella","champiniones","pepperoni","tomate"],img: "./images/pizza_PeperoniChampinones.jpg", precio: 650},
-    {id: 6, nombre:"Albahaca y cherry", ingredientes:["mozzarella","albahaca","tomate cherry"],img:"./images/pizza_AlbahacaCherry.jpg", precio:550},
+    {id: 1, nombre: "Margarita", ingredientes: ["Mozzarella","Salsa","Albahaca","Tomate"],img: "./images/pizza_Margarita.jpg", precio: 500 },
+    {id: 2, nombre:"Cuatro quesos", ingredientes: ["Mozzarella","Queso azul","Parmesano","Provolone"],img: "./images/pizza_CuatroQuesos.jpg", precio: 600},
+    {id: 3, nombre:"Jamon cocido", ingredientes:["Mozzarella","Salsa","Jamon cocido"],img: "./images/pizza_JamonCocido.jpg", precio: 800},
+    {id: 4, nombre:"Hawaiana", ingredientes:["Mozzarella","Salsa","Jamon","Piña"],img: "./images/pizza_Hawaiana.jpg", precio: 750},
+    {id: 5, nombre:"Champiniones y pepperoni", ingredientes:["Mozzarella","Champiniones","Pepperoni","Tomate"],img: "./images/pizza_PeperoniChampinones.jpg", precio: 650},
+    {id: 6, nombre:"Albahaca y cherry", ingredientes:["Mozzarella","Albahaca","Tomate cherry"],img:"./images/pizza_AlbahacaCherry.jpg", precio:550},
 ];
 
 localStorage.setItem("Pizzas", JSON.stringify(Pizzas))
@@ -28,8 +28,13 @@ function FindID (id) {
         console.log(resultado);
         PizzaName.textContent = resultado.nombre;
         PizzaImage.setAttribute("src", resultado.img);
-        PizzaIngredients.textContent = resultado.ingredientes.map(ing => ing);
-        PizzaPrice.textContent = resultado.precio;
+        PizzaIngredients.innerHTML = "";
+        resultado.ingredientes.map(ing => {
+            const list = document.createElement("li");
+            list.textContent = `- ${ing}`;
+            PizzaIngredients.appendChild(list);
+        })
+        PizzaPrice.textContent = `${resultado.precio} $`;
         ErrorMessage.classList.add("error-delete");
         ErrorMessage.classList.remove("error-message");
         ErrorMessage.textContent = "";
