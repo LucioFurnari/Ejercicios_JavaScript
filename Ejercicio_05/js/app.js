49eebbbccd2cdb6497369c0ad812dba98c0693b9
LucioFurnari <luciofurnari99@gmail.com>
2022-06-30 17:53:17 -0300

    Agregand style y funciones

--- Ejercicio_05/js/app.js
+++ Ejercicio_05/js/app.js
@@ -1,8 +1,18 @@
 import { Pizzas } from "./db.js";
-import {createCard } from "./card.js";
+import { createCard } from "./card.js";
 
+const searchForm = document.querySelector(".search-pizza");
+const searchInput = document.querySelector(".search-input");
+////////
 localStorage.setItem("Pizzas",JSON.stringify(Pizzas));
-
 const PizzaArray = JSON.parse(localStorage.getItem("Pizzas"));
 console.log(PizzaArray);
-PizzaArray.map(pizza => createCard(pizza));
+// PizzaArray.map(pizza => createCard(pizza));
+
+searchForm.addEventListener("submit" ,(e) => {
+    e.preventDefault()
+    let input = searchInput.value
+    console.log(input);
+    let name = PizzaArray.find(elem => elem.nombre == input);
+    console.log(name);
+})
--- Ejercicio_05/js/card.js
+++ Ejercicio_05/js/card.js
@@ -3,11 +3,15 @@ const container = document.querySelector(".container-card");
 
 export function createCard(elem) {
     const card = document.createElement("div");
+    card.classList.add("item");
     let ingredients = elem.ingredientes;
+
+    console.log(ingredients);
     card.innerHTML = `
-        <img src:"${elem.img}">
+        <img src=${elem.img}>
         <h2>${elem.nombre}</h2>
-        <ul>${ingredients.map(i => `<li>${i}</li>`)}</ul>
+        <ul>${ingredients.map(i => `<li>${i}</li>`).join('')}</ul>
+        <p>${elem.precio} $</p>
         `;
     container.appendChild(card);
 }
